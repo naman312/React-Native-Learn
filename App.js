c571b80c459f429367710552e46bbe5386563fc7
Naman Arora <naman.arora@citymall.live>
2021-09-02 15:03:20 +0530

    a doubt among React blur and react useFocusEffect

--- App.js
+++ App.js
@@ -1,29 +1,52 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-// In App.js in a new project
-
 import * as React from 'react';
-import { Button, View, Text, TextInput, Image } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
+import { Button, View, Text } from 'react-native';
+import { NavigationContainer, useFocusEffect } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Cool from './android/app/src/assets/pic.png'
-function LogoTitle({navigation}) {
-  
-  return (
-    <View>
-      <Image
-        style={{ width: 50, height: 50 }}
-        source={Cool}
-      />
-      <Button title="back" onPress={() => navigation.push('Profile')}/>
-    </View>
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
+function SettingsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Settings Screen</Text>
+      <Button
+        title="Go to Profile"
+        onPress={() => navigation.navigate('Profile')}
+      />
+    </View>
+  );
+}
+// Doubted section of Profile screen
+function ProfileScreen({ navigation }) {
+  useFocusEffect(
+    React.useCallback(() => {
+      alert('Screen was focused');
+      // Do something when the screen is focused
+      return () => {
+        alert('Screen was unfocused');
+        // Do something when the screen is unfocused
+        // Useful for cleanup functions
+      };
+    }, [])
+  );
+
+  // React.useEffect(
+  //   () => navigation.addListener('focus', () => alert('Screen was focused')),
+  //   []
+  // );
+
+  // React.useEffect(
+  //   () => navigation.addListener('blur', () => alert('Screen was unfocused')),
+  //   []
+  // );
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Profile Screen</Text>
+      <Button
+        title="Go to Settings"
+        onPress={() => navigation.navigate('Settings')}
+      />
+    </View>
   );
 }
 
@@ -31,93 +54,53 @@ function HomeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
-      <Button title="hey" onPress={() => navigation.push('Profile')} />
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
     </View>
   );
 }
 
-
-function ProfileScreen({ navigation }) {
+function DetailsScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Profile screen</Text>
-      <Button title="Go back" onPress={() => navigation.goBack()} />
+      <Text>Details Screen</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.push('Details')}
+      />
     </View>
   );
 }
+const Tab = createBottomTabNavigator();
+const SettingsStack = createNativeStackNavigator();
+const HomeStack = createNativeStackNavigator();
 
-
-// function DetailsScreen({ route, navigation }) {
-//   /* 2. Get the param */
-//   const { itemId, otherParam } = route.params;
-//   return (
-//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-//       <Text>Details Screen</Text>
-//       <Text>itemId: {JSON.stringify(itemId)}</Text>
-//       <Text>otherParam: {JSON.stringify(otherParam)}</Text>
-//       <Button
-//         title="Go to Details... again"
-//         onPress={() =>
-//           navigation.push('Details', {
-//             itemId: Math.floor(Math.random() * 100),
-//           })
-//         }
-//       />
-//       <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
-//       <Button title="Go back" onPress={() => navigation.goBack()} />
-//     </View>
-//   );
-// }
-
-// function CreatePostScreen({ navigation, route }) {
-//   const [postText, setPostText] = React.useState('');
-
-//   return (
-//     <>
-//       <TextInput
-//         multiline
-//         placeholder="What's on your mind?"
-//         style={{ height: 200, padding: 10, backgroundColor: 'white' }}
-//         value={postText}
-//         onChangeText={setPostText}
-//       />
-//       <Button
-//         title="Done"
-//         onPress={() => {
-//           // Pass and merge params back to home screen
-//           navigation.navigate({
-//             name: 'Home',
-//             params: { post: postText },
-//             merge: true,
-//           });
-//         }}
-//       />
-//     </>
-//   );
-// }
-
-const Stack = createNativeStackNavigator();
-
-function App() {
+export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator cool={"text"}>
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          initialParams={{ param: 'hello' }}
-          options={({ navigation, route }) => ({
-            headerTitle: props => <LogoTitle {...props} navigation={navigation}/>,
-          })}
-        />
-        <Stack.Screen
-          name="Profile"
-          component={ProfileScreen}
-        />
-        
-      </Stack.Navigator>
+      <Tab.Navigator screenOptions={{ headerShown: false }}>
+        <Tab.Screen name="First">
+          {() => (
+            <SettingsStack.Navigator>
+              <SettingsStack.Screen
+                name="Settings"
+                component={SettingsScreen}
+              />
+              <SettingsStack.Screen name="Profile" component={ProfileScreen} />
+            </SettingsStack.Navigator>
+          )}
+        </Tab.Screen>
+        <Tab.Screen name="Second">
+          {() => (
+            <HomeStack.Navigator>
+              <HomeStack.Screen name="Home" component={HomeScreen} />
+              <HomeStack.Screen name="Details" component={DetailsScreen} />
+            </HomeStack.Navigator>
+          )}
+        </Tab.Screen>
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
-
-export default App;

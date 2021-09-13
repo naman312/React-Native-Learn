25198a955a6b930fd45594965a35ae132b9a02ea
Naman Arora <naman.arora@citymall.live>
2021-09-13 11:49:11 +0530

    added image slider box

--- App.js
+++ App.js
@@ -1,36 +1,53 @@
 import * as React from 'react';
-import { Button, Text, View, StyleSheet, FlatList } from 'react-native';
+import { Button, Text, View, StyleSheet, FlatList, Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import Item from './Components/Items'
+import Item from './Components/Items';
+import banner from './android/app/src/assets/banner.jpg';
+import { SliderBox } from "react-native-image-slider-box";
+import { ScrollView } from 'react-native-gesture-handler';
+
+const images = ["https://source.unsplash.com/1024x768/?nature",
+  "https://source.unsplash.com/1024x768/?water",
+  "https://source.unsplash.com/1024x768/?girl",
+  "https://source.unsplash.com/1024x768/?tree"]
+
+
 //home screeen displayed on home
 function Home({ navigation }) {
   return (
+    <ScrollView>
+      {/* // gome screen container for the list of items */}
+      <View style={styles.container}>
+        <FlatList data={[1, 5, 6]} renderItem={() => (<Item />)}
+          horizontal={true}
+        />
+      </View>
+      <View>
+        <Text>I am in</Text>
+        <Image
+          source={banner}
+          style={styles.img}
+        />
+      </View>
+      <SliderBox
+        images={images}
+        sliderBoxHeight={200}
+        // onCurrentImagePressed={index => console.warn(`image ${index} pressed`)}
+        dotColor="#FFEE58"
+        inactiveDotColor="#90A4AE"
+      />
 
-    // gome screen container for the list of items
-    <View style={styles.container}>
-
-
-
-    {/* <Flatlist data={[{name: 'naman'},{name: 'cool'}]}
-    renderItem={()=>{
-      return ( <Item/>) 
-       }}/>
-    */} 
-    <FlatList data={[1,5,6]} renderItem={()=>(<Item/>)} 
-     horizontal={true}
-    />
-    
-  
-</View>
-
-)
+      {/* // now moving towards banner thing */}
+    </ScrollView>
+  )
 }
 
 // drawer container
 function HomeScreen({ navigation }) {
   return (
+
     <Drawer.Navigator initialRouteName="Home">
       <Drawer.Screen name="Home" component={Home} />
       <Drawer.Screen name="settings" component={SettingsScreen} />
@@ -55,7 +72,8 @@ const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
+ 
+     <NavigationContainer>
       <Tab.Navigator>
         <Tab.Screen name="HomeDrawer" component={HomeScreen} />
         <Tab.Screen name="Settings" component={SettingsScreen} />
@@ -69,6 +87,9 @@ export default function App() {
 
 
     </NavigationContainer>
+
+  
+   
   );
 }
 
@@ -78,10 +99,14 @@ const styles = StyleSheet.create(
       marginLeft: 0,
       backgroundColor: 'white',
       flexDirection: 'row',
-
       height: 250,
-
-
+    },
+    img: {
+      height: 200,
+      width: 500,
+      resizeMode: 'stretch'
     }
+
+
   }
 )
